05e6393cab8f2bb569bd1cd71e68e2e569c8a63b
collin <me@collin.sh>
2024-03-21 13:11:08 -0700

    fix buffer

--- buffer.py
+++ buffer.py
@@ -1,6 +1,7 @@
 import torch
 import datasets
 from transformer_lens import HookedTransformer
+from math import ceil
 
 
 class ActivationsBufferConfig:
@@ -9,11 +10,12 @@ class ActivationsBufferConfig:
             model_name,
             layers,
             dataset_name,
-            act_site="mlp_out",
+            act_site="mlp.hook_pre",
             dataset_split=None,
             buffer_size=256,
             min_capacity=128,
             model_batch_size=8,
+            samples_per_seq=16,
             act_size=None,
             device="cuda",
             dtype=torch.bfloat16,
@@ -28,6 +30,7 @@ class ActivationsBufferConfig:
         :param min_capacity: the minimum guaranteed capacity of the buffer, in number of activations, used to determine
         when to refresh the buffer
         :param model_batch_size: the batch size to use in the model when generating activations
+        :param samples_per_seq: the number of activations to randomly sample from each sequence
         :param act_size: the size of the activations vectors. If None, it will use the size provided by the model's cfg
         :param device: the device to use for the buffer and model
         :param dtype: the dtype to use for the buffer and model
@@ -38,11 +41,12 @@ class ActivationsBufferConfig:
         self.model_name = model_name
         self.layers = layers
         self.dataset_name = dataset_name
-        self.act_names = [act_site + str(layer) for layer in layers]  # the tl keys to grab activations from todo
+        self.act_names = [f"blocks.{layer}.{act_site}" for layer in layers]  # the tl keys to grab activations from todo
         self.dataset_split = dataset_split
         self.buffer_size = buffer_size
         self.min_capacity = min_capacity
         self.model_batch_size = model_batch_size
+        self.samples_per_seq = samples_per_seq
         self.act_size = act_size
         self.device = device
         self.dtype = dtype
@@ -62,12 +66,6 @@ class ActivationsBuffer:
 
     def __init__(self, cfg: ActivationsBufferConfig, hf_model=None):
         self.cfg = cfg
-        # the buffer to store activations in, with shape (buffer_size, len(layers), act_size)
-        self.buffer = torch.zeros((cfg.buffer_size, len(self.cfg.layers), cfg.act_size), dtype=cfg.dtype).to(cfg.device)
-
-        # pointer to read/write location in the buffer, reset to 0 after refresh is called
-        # starts at buffer_size to be fully filled on first refresh
-        self.buffer_pointer = self.cfg.buffer_size
 
         # pointer to the current position in the dataset
         self.dataset_pointer = 0
@@ -87,6 +85,13 @@ class ActivationsBuffer:
         if cfg.act_size is None:
             self.cfg.act_size = self.model.cfg.d_mlp
 
+        # the buffer to store activations in, with shape (buffer_size, len(layers), act_size)
+        self.buffer = torch.zeros((cfg.buffer_size, len(self.cfg.layers), cfg.act_size), dtype=cfg.dtype).to(cfg.device)
+
+        # pointer to read/write location in the buffer, reset to 0 after refresh is called
+        # starts at buffer_size to be fully filled on first refresh
+        self.buffer_pointer = self.cfg.buffer_size
+
         # initial buffer fill
         self.refresh()
 
@@ -98,13 +103,13 @@ class ActivationsBuffer:
         activations from the model.
         """
 
-        # remove the first `buffer_pointer` activations that were used
-        self.buffer[:self.buffer_pointer] = self.buffer[self.buffer_pointer:]
+        # shift the remaining activations to the start of the buffer
+        self.buffer = torch.roll(self.buffer, -self.buffer_pointer, 0)
 
         # fill the rest of the buffer with `buffer_pointer` new activations from the model
         while self.buffer_pointer > 0:
             # if we have less than a full `model_batch_size` left to get, use the remaining sequences
-            batch_size = min(self.buffer_pointer, self.cfg.model_batch_size)
+            batch_size = min(ceil(self.buffer_pointer/self.cfg.samples_per_seq), self.cfg.model_batch_size)
 
             # get the next batch of text from the dataset (batch_size, seq_len)
             seqs = self.dataset['text'][self.dataset_pointer:self.dataset_pointer + batch_size]
@@ -113,20 +118,24 @@ class ActivationsBuffer:
             self.dataset_pointer = (self.dataset_pointer + batch_size) % len(self.dataset['text'])
 
             # run the seqs through the model to get the activations
-            out, cache = self.model.run_with_cache(seqs, stop_at_layer=self.cfg.final_layer,
+            out, cache = self.model.run_with_cache(seqs, stop_at_layer=self.cfg.final_layer+1,
                                                    names_filter=self.cfg.act_names)
 
             # clean up logits in order to free the graph memory
             del out
             torch.cuda.empty_cache()
 
-            # store the activations (batch_size, len(layers), act_size) in the buffer
-            acts = torch.stack([cache[name] for name in self.cfg.act_names], dim=1)
+            # store the activations in the buffer
+            acts = torch.stack([cache[name] for name in self.cfg.act_names], dim=-2)
+            # (batch, pos, layers, act_size) -> (batch*samples_per_seq, layers, act_size)
+            acts = acts[:, torch.randperm(acts.shape[-3])[:self.cfg.samples_per_seq]].flatten(0, 1)
             write_pointer = self.cfg.buffer_size - self.buffer_pointer
-            self.buffer[write_pointer:write_pointer + batch_size] = acts
 
-            # update the buffer pointer
-            self.buffer_pointer -= batch_size
+            new_acts = min(acts.shape[0], self.buffer_pointer)  # the number of acts to write, capped by buffer_pointer
+            self.buffer[write_pointer:write_pointer + acts.shape[0]] = acts[:new_acts]
+
+            # update the buffer pointer by the number of activations we just added
+            self.buffer_pointer -= new_acts
 
         assert self.buffer_pointer == 0, "Buffer pointer should be 0 after refresh"
 
