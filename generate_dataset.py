cdfe5e78d0b08e6681f69b86ea9f697f7cb9905f
collin <me@collin.sh>
2024-03-23 13:36:44 -0700

    simplify dset gen script

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,27 +1,26 @@
 from buffer import *
 from tqdm import tqdm
-from datasets.arrow_writer import ArrowWriter
+import torch
 
 dataset_name = "roneneldan/TinyStories"
+total_acts = 2e10
+save_batch = 2**19
+save_dir = "./activations"
+save_name = "activations"
 
 buffer_cfg = ActivationsBufferConfig(
     model_name="mistralai/Mistral-7B-Instruct-v0.1",
     layers=[0],
     dataset_name=dataset_name,
     dataset_split="train",
-    buffer_size=2**19,
+    buffer_size=save_batch,
+    min_capacity=0,
     buffer_device="cpu",
-    model_batch_size=8,
+    model_batch_size=16,
     samples_per_seq=64,
 )
 buffer = ActivationsBuffer(buffer_cfg)
 
-total_acts = 2e10
-save_batch = 2**19
-
-with ArrowWriter(path=f"{dataset_name}_acts.arrow") as writer:
-    for i in tqdm(range(total_acts // save_batch)):
-        acts = buffer.next(batch=save_batch)
-        writer.write(acts.numpy())
-
-writer.finalize()
+for i in tqdm(range(int(total_acts) // save_batch)):
+    acts = buffer.next(batch=save_batch)
+    torch.save(acts, f"{save_dir}/{save_name}_{i}.pt")
